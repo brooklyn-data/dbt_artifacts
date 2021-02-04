923e863cd5ecd7d689503995803ef7ca316561f5
Claire Carroll <20294432+clrcrl@users.noreply.github.com>
2021-02-04 16:40:15 -0500

    Make upload_artifacts work with package

--- macros/upload_artifacts.sql
+++ macros/upload_artifacts.sql
@@ -1,33 +1,36 @@
-{% macro upload_dbt_artifacts(table) %}
-{% set filename %}
-    {%- if table == 'manifests' -%}
-        target/manifest.json
-    {%- elif table == 'run_results' or table =='test_run_results' -%}
-        target/run_results.json
-    {%- else -%}
-        {{ exceptions.raise_compiler_error("Invalid tablename passed. Got: " ~ table ~ " must be one of [manifests, run_results]") }}
-    {%- endif -%}
-{% endset %}
-{% set put_query %}
-    PUT file://{{ filename }} @{{ database }}.DBT.DBT_LOAD auto_compress=true;
-{% endset %}
-{% set copy_query %}
-    BEGIN;
-    COPY INTO {{ database }}.dbt.{{ table | upper }} FROM
-        @{{ database }}.DBT.DBT_LOAD
-        file_format=(type='JSON')
-        on_error='skip_file';
-    COMMIT;
-{% endset %}
-{% set remove_query %}
-    REMOVE @{{ database }}.DBT.DBT_LOAD pattern='.*.json.gz';
-{% endset %}
-{% do log("Clearing files from Stage: " ~ remove_query, info=True) %}
-{% do run_query(remove_query) %}
-{% do log("Uploading file to Stage: " ~ put_query, info=True) %}
-{% do run_query(put_query) %}
-{% do log("Copying artifact from Stage: " ~ copy_query, info=True) %}
-{% do run_query(copy_query) %}
-{% do log("Removing artifact from Stage: " ~ remove_query, info=True) %}
-{% do run_query(remove_query) %}
+{% macro upload_dbt_artifacts() %}
+{% for file in ['manifest', 'run_results'] %}
+    {% set put_query %}
+        put file://target/{{ file }}.json @{{ var("dbt_artifacts_database") }}.{{ var("dbt_artifacts_schema") }}.artifacts_load auto_compress=true;
+    {% endset %}
+    {% set copy_query %}
+        begin;
+        copy into {{ var("dbt_artifacts_database") }}.{{ var("dbt_artifacts_schema") }}.{{ var("dbt_artifacts_table") }} from
+            (
+                select
+                $1 as data,
+                $1:metadata:generated_at::timestamp_ntz as generated_at,
+                metadata$filename as path,
+                regexp_substr(metadata$filename, '([a-z_]+.json)$') as artifact_type
+                from  @{{ var("dbt_artifacts_database") }}.{{ var("dbt_artifacts_schema") }}.artifacts_load
+            )
+            file_format=(type='JSON')
+            on_error='skip_file';
+        commit;
+    {% endset %}
+    {% set remove_query %}
+        remove @{{ var("dbt_artifacts_database") }}.{{ var("dbt_artifacts_schema") }}.artifacts_load pattern='.*.json.gz';
+    {% endset %}
+    {% do log("Clearing files from Stage: " ~ remove_query, info=True) %}
+    {% do run_query(remove_query) %}
+    {% do log("Uploading " ~ file ~ " to Stage: " ~ put_query, info=True) %}
+    {% do run_query(put_query) %}
+    {% do log("Copying " ~ file ~ " from Stage: " ~ copy_query, info=True) %}
+    {% do run_query(copy_query) %}
+    {% do log("Removing " ~ file ~ " from Stage: " ~ remove_query, info=True) %}
+    {% do run_query(remove_query) %}
+
+{% endfor %}
+
 {% endmacro %}
+
