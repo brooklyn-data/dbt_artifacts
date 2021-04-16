4c8fe6a4043e8fbd262246c4df6d994bcd492366
Konrad <77880496+kon-mal@users.noreply.github.com>
2021-04-16 12:34:09 +0100

    Capturing exposures data (#18)
    * Added three models to capture exposures data: stg, dim and fct.
    
    * Refactored fct model to adhere to dbt style guide.

---
+++ models/incremental/dim_dbt__exposures.sql
@@ -0,0 +1,44 @@
+{{ 
+  config(
+    materialized='incremental', 
+    unique_key='manifest_model_id'
+    ) 
+}}
+
+with dbt_models as (
+
+    select * from {{ ref('stg_dbt__exposures') }}
+
+),
+
+dbt_models_incremental as (
+
+    select *
+    from dbt_models
+
+    {% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    where artifact_generated_at > (select max(artifact_generated_at) from {{ this }})
+    {% endif %}
+
+),
+
+fields as (
+
+     select 
+        t.manifest_model_id,
+        t.command_invocation_id,
+        t.artifact_generated_at,
+        t.node_id,
+        t.name,
+        t.type,
+        t.owner,
+        t.maturity,
+        f.value::string as output_feeds,
+        t.package_name
+    from dbt_models_incremental t,
+    lateral flatten(input => depends_on_nodes) f
+
+)
+
+select * from fields
