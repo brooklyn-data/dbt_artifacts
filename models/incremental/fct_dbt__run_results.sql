9f45f18003aad07a1aa28a20e267660e62fc6a44
Alan <alanmcruickshank@gmail.com>
2021-05-02 20:11:21 +0100

    jinja syntax

--- models/incremental/fct_dbt__run_results.sql
+++ models/incremental/fct_dbt__run_results.sql
@@ -34,7 +34,7 @@ fields as (
         was_full_refresh
 
         {% if env_keys %}
-        {% for key in sorted(env_keys) %}
+        {% for key in env_keys|sort %}
         ,env:{{ key }} as env_{{ key }}
         {% endfor %}
         {% endif %}
