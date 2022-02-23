a8d1755eb94972e68601bdda3d21ec02b4bbc6d3
Alan <alanmcruickshank@gmail.com>
2022-02-23 09:50:29 +0000

    add schema name and database into the variable.

--- macros/create_artifact_resources.sql
+++ macros/create_artifact_resources.sql
@@ -1,7 +1,7 @@
 {% macro create_artifact_resources() %}
 
 {% set src_dbt_artifacts = source('dbt_artifacts', 'artifacts') %}
-{% set artifact_stage = var('dbt_artifacts_stage', 'dbt_artifacts_stage') %}
+{% set artifact_stage = src_dbt_artifacts.database ~ "." ~ {{ src_dbt_artifacts.schema }} ~ "." ~ var('dbt_artifacts_stage', 'dbt_artifacts_stage') %}
 
 {% set src_results = source('dbt_artifacts', 'dbt_run_results') %}
 {% set src_results_nodes = source('dbt_artifacts', 'dbt_run_results_nodes') %}
@@ -15,7 +15,7 @@ file_format = (type = json);
 {% endset %}
 
 {% set create_v2_stage_query %}
-create stage if not exists {{ src_dbt_artifacts.database }}.{{ src_dbt_artifacts.schema }}.{{ artifact_stage }}
+create stage if not exists {{ artifact_stage }}
 file_format = (type = json);
 {% endset %}
 
