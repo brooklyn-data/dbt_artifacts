9e520d41c5c97844e44169d2e9ae7af1a053f98e
Alan <alanmcruickshank@gmail.com>
2022-03-11 10:30:22 +0000

    Dedupe on the correct granularity

--- macros/dedupe_artifacts_v2.sql
+++ macros/dedupe_artifacts_v2.sql
@@ -4,14 +4,18 @@
 {% set src_results_nodes = source('dbt_artifacts', 'dbt_run_results_nodes') %}
 {% set src_manifest_nodes = source('dbt_artifacts', 'dbt_manifest_nodes') %}
 
-{% for artifact_table in [src_results, src_results_nodes, src_manifest_nodes] %}
+{% for artifact_table, table_key in [
+    (src_results, 'command_invocation_id'),
+    (src_results_nodes, 'command_invocation_id, node_id'),
+    (src_manifest_nodes, 'command_invocation_id, node_id')
+] %}
 
     {% set dedupe_results_query %}
 
         create temporary table {{ artifact_table.database }}.{{ artifact_table.schema }}.dbt_temp_artifact_table as
             select * from {{ artifact_table }}
             qualify row_number() over (
-                partition by command_invocation_id
+                partition by {{ table_key }}
                 -- NB: Snowflake requires an order by clause, although all rows will be the same within a partition.
                 order by artifact_generated_at
             ) = 1;
