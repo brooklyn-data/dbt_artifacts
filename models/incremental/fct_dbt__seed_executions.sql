a664b114fabf22e4bc343dc440cf89754b7046fa
Alan <alanmcruickshank@gmail.com>
2022-03-20 20:57:24 +0000

    Back to the layout which *should* work

--- models/incremental/fct_dbt__seed_executions.sql
+++ models/incremental/fct_dbt__seed_executions.sql
@@ -19,10 +19,10 @@ seed_executions_incremental as (
     select *
     from node_executions
     where resource_type = 'seed'
-    {% if is_incremental() %}
-        -- this filter will only be applied on an incremental run
-        and coalesce(artifact_generated_at > (select max(artifact_generated_at) from {{ this }}), true)
-    {% endif %}
+        {% if is_incremental() %}
+            -- this filter will only be applied on an incremental run
+            and coalesce(artifact_generated_at > (select max(artifact_generated_at) from {{ this }}), true)
+        {% endif %}
 
 ),
 
