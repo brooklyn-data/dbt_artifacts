7e2611843ceb678ddc702083887027891082a0b0
Daniel Palma <daniel.palma@brooklyndata.co>
2022-07-15 10:45:24 +0200

    Seed executions model

---
+++ macros/upload_seed_executions.sql
@@ -0,0 +1,66 @@
+{% macro upload_seed_executions(results) -%}
+    {% set seeds = [] %}
+    {% for result in results  %}
+        {{ log("KAKI") }}
+        {% if result.node.resource_type == "seed" %}
+            {% do seeds.append(result) %}
+        {% endif %}
+    {% endfor %}
+    {% if seeds != [] %}
+        {% set src_dbt_seed_executions = source('dbt_artifacts', 'seed_executions') %}
+        {{ dbt_artifacts.create_seed_executions_table_if_not_exists(src_dbt_seed_executions.schema, src_dbt_seed_executions.identifier) }}
+
+        {% set seed_execution_values %}
+        {% for model in results if model.node.resource_type == "seed" -%}
+            (
+                '{{ invocation_id }}', {# command_invocation_id #}
+                '{{ model.node.unique_id }}', {# node_id #}
+
+                {% set config_full_refresh = model.node.config.full_refresh %}
+                {% if config_full_refresh is none %}
+                    {% set config_full_refresh = flags.FULL_REFRESH %}
+                {% endif %}
+                '{{ config_full_refresh }}', {# was_full_refresh #}
+
+                '{{ model.thread_id }}', {# thread_id #}
+                '{{ model.status }}', {# status #}
+
+                {% if model.timing != [] %}
+                    {% for stage in model.timing if stage.name == "compile" %}
+                        {% if loop.length == 0 %}
+                            null, {# compile_started_at #}
+                        {% else %}
+                            '{{ stage.started_at }}', {# compile_started_at #}
+                        {% endif %}
+                    {% endfor %}
+
+                    {% for stage in model.timing if stage.name == "execute" %}
+                        {% if loop.length == 0 %}
+                            null, {# query_completed_at #}
+                        {% else %}
+                            '{{ stage.completed_at }}', {# query_completed_at #}
+                        {% endif %}
+                    {% endfor %}
+                {% else %}
+                    null, {# compile_started_at #}
+                    null, {# query_completed_at #}
+                {% endif %}
+
+                {{ model.execution_time }}, {# total_node_runtime #}
+                null, -- rows_affected not available {# Databricks #}
+                '{{ model.node.config.materialized }}', {# materialization #}
+                '{{ model.node.schema }}', {# schema #}
+                '{{ model.node.name }}' {# name #}
+            )
+            {%- if not loop.last %},{%- endif %}
+        {%- endfor %}
+        {% endset %}
+
+        {{ dbt_artifacts.insert_into_metadata_table(
+            schema_name=src_dbt_seed_executions.schema,
+            table_name=src_dbt_seed_executions.identifier,
+            content=seed_execution_values
+            )
+        }}
+    {% endif %}
+{% endmacro -%}
