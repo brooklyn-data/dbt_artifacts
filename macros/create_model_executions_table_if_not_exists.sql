c2c7b6ef47bf8cd1c9101aedc57f75029466718d
Michael Fagan <michael.g.fagan@gmail.com>
2022-08-16 15:57:04 -0500

    Remove _test suffix from macros

--- macros/create_model_executions_table_if_not_exists.sql
+++ macros/create_model_executions_table_if_not_exists.sql
@@ -38,7 +38,7 @@
     using delta
 {%- endmacro %} #}
 
-{% macro snowflake__get_create_model_executions_table_if_not_exists_statement_test(database_name, schema_name, table_name) -%}
+{% macro snowflake__get_create_model_executions_table_if_not_exists_statement(database_name, schema_name, table_name) -%}
     create table {{database_name}}.{{schema_name}}.{{table_name}} (
         command_invocation_id STRING,
         node_id STRING,
@@ -60,7 +60,7 @@
     )
 {%- endmacro %}
 
-{% macro default__get_create_model_executions_table_if_not_exists_statement_test(database_name, schema_name, table_name) -%}
+{% macro default__get_create_model_executions_table_if_not_exists_statement(database_name, schema_name, table_name) -%}
     create table {{database_name}}.{{schema_name}}.{{table_name}} (
         command_invocation_id STRING,
         node_id STRING,
