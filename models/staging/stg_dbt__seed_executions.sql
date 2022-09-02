d0b7fab4482fc250a90be421adc9563857a5a611
Josh Devlin <josh.devlin@brooklyndata.co>
2022-09-02 12:25:19 +1000

    Remove dependency on dbt_utils

--- models/staging/stg_dbt__seed_executions.sql
+++ models/staging/stg_dbt__seed_executions.sql
@@ -8,12 +8,12 @@ with base as (
 enhanced as (
 
     select
-        {{ dbt_utils.surrogate_key(['command_invocation_id', 'node_id']) }} as seed_execution_id,
+        {{ dbt_artifacts.surrogate_key(['command_invocation_id', 'node_id']) }} as seed_execution_id,
         command_invocation_id,
         node_id,
         run_started_at,
         was_full_refresh,
-        {{ dbt_utils.split_part('thread_id', "'-'", 2) }} as thread_id, -- noqa
+        {{ split_part('thread_id', "'-'", 2) }} as thread_id,
         status,
         compile_started_at,
         query_completed_at,
--- models/staging/stg_dbt__sources.sql
+++ models/staging/stg_dbt__sources.sql
@@ -8,7 +8,7 @@ with base as (
 enhanced as (
 
     select
-        {{ dbt_utils.surrogate_key(['command_invocation_id', 'node_id']) }} as source_execution_id,
+        {{ dbt_artifacts.surrogate_key(['command_invocation_id', 'node_id']) }} as source_execution_id,
         command_invocation_id,
         node_id,
         run_started_at,
