0fe53c0ac20d3107e8ce679640d57a154c440438
charles verleyen <charles@astrafy.io>
2022-10-14 12:49:52 +0200

    Add backticks to BigQuery table IDs fo insertion as otherwise it throws an error in case table name has a '-'

--- macros/insert_into_metadata_table.sql
+++ macros/insert_into_metadata_table.sql
@@ -25,7 +25,7 @@
 {% macro bigquery__insert_into_metadata_table(database_name, schema_name, table_name, content) -%}
 
         {% set insert_into_table_query %}
-        insert into {{database_name}}.{{ schema_name }}.{{ table_name }}
+        insert into `{{database_name}}.{{ schema_name }}.{{ table_name }}`
         VALUES
         {{ content }}
         {% endset %}
