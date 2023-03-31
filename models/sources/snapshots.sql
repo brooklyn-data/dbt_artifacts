30957d8ac80c6c61cd6ca0fb0955bf3f9bc90ef7
Gemma Down <gemma.down@brooklyndata.co>
2023-03-31 12:35:01 +0100

    Add snapshot

--- models/sources/snapshots.sql
+++ models/sources/snapshots.sql
@@ -16,6 +16,7 @@ select
     cast(null as {{ type_string() }}) as checksum,
     cast(null as {{ type_string() }}) as strategy,
     cast(null as {{ type_json() }}) as meta,
-    cast(null as {{ type_string() }}) as alias
+    cast(null as {{ type_string() }}) as alias,
+    cast(null as {{ type_json() }}) as all_results
 from dummy_cte
 where 1 = 0
