01ddad60947ac696789140cd22aad195896b17f9
Nadya Hrebinka <nadya.hrebinka@wexinc.com>
2023-05-05 12:14:47 -0400

    reset indent back to orig

--- macros/upload_tests.sql
+++ macros/upload_tests.sql
@@ -39,19 +39,19 @@
 {% macro bigquery__get_tests_dml_sql(tests) -%}
     {% if tests != [] %}
         {% set test_values %}
-        {% for test in tests -%}
-            (
-                '{{ invocation_id }}', {# command_invocation_id #}
-                '{{ test.unique_id }}', {# node_id #}
-                '{{ run_started_at }}', {# run_started_at #}
-                '{{ test.name }}', {# name #}
-                {{ tojson(test.depends_on.nodes) }}, {# depends_on_nodes #}
-                '{{ test.package_name }}', {# package_name #}
-                '{{ test.original_file_path | replace('\\', '\\\\') }}', {# test_path #}
-                {{ tojson(test.tags) }} {# tags #}
-            )
-            {%- if not loop.last %},{%- endif %}
-        {%- endfor %}
+            {% for test in tests -%}
+                (
+                    '{{ invocation_id }}', {# command_invocation_id #}
+                    '{{ test.unique_id }}', {# node_id #}
+                    '{{ run_started_at }}', {# run_started_at #}
+                    '{{ test.name }}', {# name #}
+                    {{ tojson(test.depends_on.nodes) }}, {# depends_on_nodes #}
+                    '{{ test.package_name }}', {# package_name #}
+                    '{{ test.original_file_path | replace('\\', '\\\\') }}', {# test_path #}
+                    {{ tojson(test.tags) }} {# tags #}
+                )
+                {%- if not loop.last %},{%- endif %}
+            {%- endfor %}
         {% endset %}
         {{ test_values }}
     {% else %}
