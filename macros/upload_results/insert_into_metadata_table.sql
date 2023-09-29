778a435ddee146002d1c34840caab42e5154a0e3
Gemma Down <gemma.down@brooklyndata.co>
2023-09-29 14:10:06 +0100

    Add missing CI tests

--- macros/upload_results/insert_into_metadata_table.sql
+++ macros/upload_results/insert_into_metadata_table.sql
@@ -45,5 +45,18 @@
 
 {%- endmacro %}
 
+{%- endmacro %}
+
+{% macro postgres__insert_into_metadata_table(database_name, schema_name, table_name, content) -%}
+
+        {% set insert_into_table_query %}
+        insert into {{ relation }} {{ fields }}
+        {{ content }}
+        {% endset %}
+
+        {% do run_query(insert_into_table_query) %}
+
+{%- endmacro %}
+
 {% macro default__insert_into_metadata_table(relation, fields, content) -%}
 {%- endmacro %}
