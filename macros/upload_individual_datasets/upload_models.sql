dff781bf18fdad10693e70cf50812ddfaba64c7a
Mark Greenwood <mark.greenwood@netacea.com>
2024-01-25 12:10:26 +0000

    replace model for include all results

--- macros/upload_individual_datasets/upload_models.sql
+++ macros/upload_individual_datasets/upload_models.sql
@@ -80,7 +80,7 @@
                     {% if var('dbt_artifacts_exclude_all_results', false) %}
                         null
                     {% else %}
-                        {{ adapter.dispatch('parse_json', 'dbt_artifacts')(tojson(model) | replace("\\", "\\\\") | replace("'","\\'") | replace('"', '\\"')) }} {# all_results #}
+                        {{ adapter.dispatch('parse_json', 'dbt_artifacts')(tojson(model_copy) | replace("\\", "\\\\") | replace("'","\\'") | replace('"', '\\"')) }} {# all_results #}
                     {% endif %}
                 )
                 {%- if not loop.last %},{%- endif %}
@@ -116,7 +116,7 @@
                     {% if var('dbt_artifacts_exclude_all_results', false) %}
                         null
                     {% else %}
-                        $${{ tojson(model) }}$$ {# all_results #}
+                        $${{ tojson(model_copy) }}$$ {# all_results #}
                     {% endif %}
                 )
                 {%- if not loop.last %},{%- endif %}
