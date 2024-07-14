4a330a7e6a2a6afc543b07ce6505761b9115baba
Doug Guthrie <douglas.p.guthrie@gmail.com>
2024-07-14 11:50:28 -0600

    Fix other adapter dml

--- macros/upload_individual_datasets/upload_source_executions.sql
+++ macros/upload_individual_datasets/upload_source_executions.sql
@@ -91,9 +91,9 @@
                 '{{ source.node.freshness.warn_after.period }}', {# warn_after_period #}
                 '{{ source.node.freshness.error_after.count }}', {# error_after_count #}
                 '{{ source.node.freshness.error_after.period }}', {# error_after_period #}
-                '{{ source.node.max_loaded_at }}', {# max_loaded_at #}
-                '{{ source.node.snapshotted_at }}', {# snapshotted_at #}
-                {{ source.node.age }}, {# age #}
+                '{{ source.max_loaded_at }}', {# max_loaded_at #}
+                '{{ source.snapshotted_at }}', {# snapshotted_at #}
+                {{ source.age }}, {# age #}
                 {{ adapter.dispatch('parse_json', 'dbt_artifacts')(tojson(model.adapter_response) | replace("\\", "\\\\") | replace("'", "\\'") | replace('"', '\\"')) }} {# adapter_response #}
             )
             {%- if not loop.last %},{%- endif %}
@@ -194,9 +194,9 @@
                 '{{ source.node.freshness.warn_after.period }}', {# warn_after_period #}
                 '{{ source.node.freshness.error_after.count }}', {# error_after_count #}
                 '{{ source.node.freshness.error_after.period }}', {# error_after_period #}
-                '{{ source.node.max_loaded_at }}', {# max_loaded_at #}
-                '{{ source.node.snapshotted_at }}', {# snapshotted_at #}
-                {{ source.node.age }}, {# age #}
+                '{{ source.max_loaded_at }}', {# max_loaded_at #}
+                '{{ source.snapshotted_at }}', {# snapshotted_at #}
+                {{ source.age }}, {# age #}
                 $${{ tojson(model.adapter_response) }}$$ {# adapter_response #}
             )
             {%- if not loop.last %},{%- endif %}
