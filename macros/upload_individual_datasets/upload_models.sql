797931889cd314eecde57f788fc18bb08f8f39c2
KijkEr <github@colerain.nl>
2024-09-18 11:01:53 +0200

    Added SQL Server specific uploads

--- macros/upload_individual_datasets/upload_models.sql
+++ macros/upload_individual_datasets/upload_models.sql
@@ -1,5 +1,5 @@
 {% macro upload_models(models) -%}
-    {{ return(adapter.dispatch('get_models_dml_sql', 'dbt_artifacts')(models)) }}
+    {{ return(adapter.dispatch("get_models_dml_sql", "dbt_artifacts")(models)) }}
 {%- endmacro %}
 
 {% macro default__get_models_dml_sql(models) -%}
@@ -51,8 +51,7 @@
         {%- endfor %}
         {% endset %}
         {{ model_values }}
-    {% else %}
-        {{ return("") }}
+    {% else %} {{ return("") }}
     {% endif %}
 {% endmacro -%}
 
@@ -87,8 +86,7 @@
             {%- endfor %}
         {% endset %}
         {{ model_values }}
-    {% else %}
-        {{ return("") }}
+    {% else %} {{ return("") }}
     {% endif %}
 {%- endmacro %}
 
@@ -123,7 +121,50 @@
             {%- endfor %}
         {% endset %}
         {{ model_values }}
-    {% else %}
-        {{ return("") }}
+    {% else %} {{ return("") }}
     {% endif %}
 {%- endmacro %}
+
+
+{% macro sqlserver__get_models_dml_sql(models) -%}
+
+    {% if models != [] %}
+        {% set model_values %}
+        select
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"
+        from ( values
+        {% for model in models -%}
+                {% set model_copy = model.copy() -%}
+                {% do model_copy.pop('raw_code', None) %}
+            (
+                '{{ invocation_id }}', {# command_invocation_id #}
+                '{{ model_copy.unique_id }}', {# node_id #}
+                '{{ run_started_at }}', {# run_started_at #}
+                '{{ model_copy.database }}', {# database #}
+                '{{ model_copy.schema }}', {# schema #}
+                '{{ model_copy.name }}', {# name #}
+                '{{ tojson(model_copy.depends_on.nodes) }}', {# depends_on_nodes #}
+                '{{ model_copy.package_name }}', {# package_name #}
+                '{{ model_copy.original_file_path }}', {# path #}
+                '{{ model_copy.checksum.checksum }}', {# checksum #}
+                '{{ model_copy.config.materialized }}', {# materialization #}
+                '{{ tojson(model_copy.tags) }}', {# tags #}
+                '{{ tojson(model_copy.config.meta) | replace("'","''") }}', {# meta #}
+                '{{ model_copy.alias }}', {# alias #}
+                {% if var('dbt_artifacts_exclude_all_results', false) %}
+                    null
+                {% else %}
+                    '{{ tojson(model_copy) | replace("'","''") }}' {# all_results #}
+                {% endif %}
+            )
+            {%- if not loop.last %},{%- endif %}
+        {%- endfor %}
+
+        ) v ("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15")
+
+        {% endset %}
+        {{ model_values }}
+    {% else %} {{ return("") }}
+    {% endif %}
+{% endmacro -%}
+
--- macros/upload_individual_datasets/upload_test_executions.sql
+++ macros/upload_individual_datasets/upload_test_executions.sql
@@ -1,5 +1,5 @@
 {% macro upload_test_executions(tests) -%}
-    {{ return(adapter.dispatch('get_test_executions_dml_sql', 'dbt_artifacts')(tests)) }}
+    {{ return(adapter.dispatch("get_test_executions_dml_sql", "dbt_artifacts")(tests)) }}
 {%- endmacro %}
 
 {% macro default__get_test_executions_dml_sql(tests) -%}
@@ -50,8 +50,7 @@
         {%- endfor %}
         {% endset %}
         {{ test_execution_values }}
-    {% else %}
-        {{ return("") }}
+    {% else %} {{ return("") }}
     {% endif %}
 {% endmacro -%}
 
@@ -89,8 +88,7 @@
         {%- endfor %}
         {% endset %}
         {{ test_execution_values }}
-    {% else %}
-        {{ return("") }}
+    {% else %} {{ return("") }}
     {% endif %}
 {% endmacro -%}
 
@@ -144,8 +142,7 @@
         {%- endfor %}
         {% endset %}
         {{ test_execution_values }}
-    {% else %}
-        {{ return("") }}
+    {% else %} {{ return("") }}
     {% endif %}
 {% endmacro -%}
 
@@ -197,7 +194,51 @@
         {%- endfor %}
         {% endset %}
         {{ test_execution_values }}
-    {% else %}
-        {{ return("") }}
+    {% else %} {{ return("") }}
     {% endif %}
 {% endmacro -%}
+
+{% macro sqlserver__get_test_executions_dml_sql(tests) -%}
+    {% if tests != [] %}
+        {% set test_execution_values %}
+        select
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"
+        from ( values
+        {% for test in tests -%}
+            (
+                '{{ invocation_id }}', {# command_invocation_id #}
+                '{{ test.node.unique_id }}', {# node_id #}
+                '{{ run_started_at }}', {# run_started_at #}
+
+                {% set config_full_refresh = test.node.config.full_refresh %}
+                {% if config_full_refresh is none %}
+                    {% set config_full_refresh = flags.FULL_REFRESH %}
+                {% endif %}
+                '{{ config_full_refresh }}', {# was_full_refresh #}
+
+                '{{ test.thread_id }}', {# thread_id #}
+                '{{ test.status }}', {# status #}
+
+                {% set compile_started_at = (model.timing | selectattr("name", "eq", "compile") | first | default({}))["started_at"] %}
+                {% if compile_started_at %}'{{ compile_started_at }}'{% else %}null{% endif %}, {# compile_started_at #}
+                {% set query_completed_at = (model.timing | selectattr("name", "eq", "execute") | first | default({}))["completed_at"] %}
+                {% if query_completed_at %}'{{ query_completed_at }}'{% else %}null{% endif %}, {# query_completed_at #}
+
+                {{ test.execution_time }}, {# total_node_runtime #}
+                null, {# rows_affected not available in Databricks #}
+                {{ 'null' if test.failures is none else test.failures }}, {# failures #}
+                '{{ test.message | replace("'", "''") }}', {# message #}
+                '{{ tojson(test.adapter_response) | replace("'", "''") }}' {# adapter_response #}
+            )
+            {%- if not loop.last %},{%- endif %}
+        {%- endfor %}
+
+        ) AS v ("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13")
+
+        {% endset %}
+        {{ test_execution_values }}
+    {% else %} {{ return("") }}
+    {% endif %}
+{% endmacro -%}
+
+
