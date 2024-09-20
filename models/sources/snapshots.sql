114b87cbc275b7081c9b53cae6eaa4c560a69321
KijkEr <github@colerain.nl>
2024-09-20 07:55:43 +0200

    Added delimitied identifier conditional for sql server

--- models/sources/snapshots.sql
+++ models/sources/snapshots.sql
@@ -1,24 +1,25 @@
 /* Bigquery won't let us `where` without `from` so we use this workaround */
-with
-    dummy_cte as (
-
-        select 1 as foo
-    )
+with dummy_cte as (select 1 as foo)
 
 select
-    cast(null as {{ type_string() }}) as command_invocation_id
-    , cast(null as {{ type_string() }}) as node_id
-    , cast(null as {{ type_timestamp() }}) as run_started_at
-    , cast(null as {{ type_string() }}) as "database"
-    , cast(null as {{ type_string() }}) as "schema"
-    , cast(null as {{ type_string() }}) as name
-    , cast(null as {{ type_array() }}) as depends_on_nodes
-    , cast(null as {{ type_string() }}) as package_name
-    , cast(null as {{ type_string() }}) as path
-    , cast(null as {{ type_string() }}) as checksum
-    , cast(null as {{ type_string() }}) as strategy
-    , cast(null as {{ type_json() }}) as meta
-    , cast(null as {{ type_string() }}) as alias
-    , cast(null as {{ type_json() }}) as all_results
+    cast(null as {{ type_string() }}) as command_invocation_id,
+    cast(null as {{ type_string() }}) as node_id,
+    cast(null as {{ type_timestamp() }}) as run_started_at
+    cast(null as {{ type_string() }}) as {% if target.type == "sqlserver" %} "database"
+    {% else %} database
+    {% endif %},
+    cast(null as {{ type_string() }}) as {% if target.type == "sqlserver" %} "schema"
+    {% else %} schema
+    {% endif %},,
+    cast(null as {{ type_string() }}) as name,
+    cast(null as {{ type_array() }}) as depends_on_nodes,
+    cast(null as {{ type_string() }}) as package_name,
+    cast(null as {{ type_string() }}) as path,
+    cast(null as {{ type_string() }}) as checksum,
+    cast(null as {{ type_string() }}) as strategy,
+    cast(null as {{ type_json() }}) as meta,
+    cast(null as {{ type_string() }}) as alias,
+    cast(null as {{ type_json() }}) as all_results
 from dummy_cte
 where 1 = 0
+
