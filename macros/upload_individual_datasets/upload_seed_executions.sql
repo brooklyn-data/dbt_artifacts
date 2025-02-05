2b6caf5f7c509c119b1e7dbb02983c4981406186
Eva Bilski <eva.bilski@mobsuccess.com>
2025-02-05 11:58:18 +0100

    Add Athena support for seeds and seed_executions

--- macros/upload_individual_datasets/upload_seed_executions.sql
+++ macros/upload_individual_datasets/upload_seed_executions.sql
@@ -60,6 +60,47 @@
     {% endif %}
 {% endmacro -%}
 
+{% macro athena__get_seed_executions_dml_sql(seeds) -%}
+    {% if seeds != [] %}
+        {% set seed_execution_values %}
+        VALUES
+        {% for model in seeds -%}
+            (
+                '{{ invocation_id }}', {# command_invocation_id #}
+                '{{ model.node.unique_id }}', {# node_id #}
+                CAST('{{ run_started_at }}' AS timestamp), {# run_started_at #}
+
+                {% set config_full_refresh = model.node.config.full_refresh %}
+                {% if config_full_refresh is none %}
+                    {% set config_full_refresh = flags.FULL_REFRESH %}
+                {% endif %}
+                {{ config_full_refresh }}, {# was_full_refresh #}
+
+                '{{ model.thread_id }}', {# thread_id #}
+                '{{ model.status }}', {# status #}
+
+                {% set compile_started_at = (model.timing | selectattr("name", "eq", "compile") | first | default({}))["started_at"] %}
+                {% if compile_started_at %}CAST('{{ compile_started_at }}' AS timestamp){% else %}CAST(null AS timestamp){% endif %}, {# compile_started_at #}
+                {% set query_completed_at = (model.timing | selectattr("name", "eq", "execute") | first | default({}))["completed_at"] %}
+                {% if query_completed_at %}CAST('{{ query_completed_at }}' AS timestamp){% else %}CAST(null AS timestamp){% endif %}, {# query_completed_at #}
+
+                {{ model.execution_time }}, {# total_node_runtime #}
+                cast({{ model.adapter_response.rows_affected }} as integer), {# rows_affected #}
+                '{{ model.node.config.materialized }}', {# materialization #}
+                '{{ model.node.schema }}', {# schema #}
+                '{{ model.node.name }}', {# name #}
+                '{{ model.node.alias }}', {# alias #}
+                '{{ model.message | replace("'", "''") }}', {# message #}
+                '{{ tojson(model.adapter_response) | replace("\'", "\'\'") }}' {# adapter_response #}
+            )
+            {%- if not loop.last %},{%- endif %}
+        {%- endfor %}
+        {% endset %}
+        {{ seed_execution_values }}
+    {% else %} {{ return("") }}
+    {% endif %}
+{% endmacro -%}
+
 {% macro bigquery__get_seed_executions_dml_sql(seeds) -%}
     {% if seeds != [] %}
         {% set seed_execution_values %}
--- macros/upload_individual_datasets/upload_seeds.sql
+++ macros/upload_individual_datasets/upload_seeds.sql
@@ -47,6 +47,38 @@
     {% endif %}
 {% endmacro -%}
 
+{% macro athena__get_seeds_dml_sql(seeds) -%}
+    {% if seeds != [] %}
+        {% set seed_values %}
+            VALUES
+            {% for seed in seeds -%}
+                (
+                    '{{ invocation_id }}', {# command_invocation_id #}
+                    '{{ seed.unique_id }}', {# node_id #}
+                    CAST('{{ run_started_at }}' AS timestamp), {# run_started_at #}
+                    '{{ seed.database }}', {# database #}
+                    '{{ seed.schema }}', {# schema #}
+                    '{{ seed.name }}', {# name #}
+                    '{{ seed.package_name }}', {# package_name #}
+                    '{{ seed.original_file_path | replace('\\', '\\\\') }}', {# path #}
+                    '{{ seed.checksum.checksum | replace('\\', '\\\\')}}', {# checksum #}
+                    '{{ tojson(seed.config.meta) | replace("\\", "\\\\") | replace("'","\\'") | replace('"', '\\"') }}', {# meta #}
+                    '{{ seed.alias }}', {# alias #}
+                    {% if var('dbt_artifacts_exclude_all_results', false) %}
+                        cast(null as varchar)
+                    {% else %}
+                        '{{ tojson(seed) | replace("\'", "\'\'") }}' {# all_results #}
+                    {% endif %}
+                )
+                {%- if not loop.last %},{%- endif %}
+            {%- endfor %}
+        {% endset %}
+        {{ seed_values }}
+    {% else %} 
+        {{ return("") }}
+    {% endif %}
+{% endmacro -%}
+
 {% macro bigquery__get_seeds_dml_sql(seeds) -%}
     {% if seeds != [] %}
         {% set seed_values %}
