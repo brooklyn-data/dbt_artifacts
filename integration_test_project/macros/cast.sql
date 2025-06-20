b0a043afb689f2a9df78a9c07e6767a1f0c16c6a
Laura Li Foa Wing <laura.lifoawing@brooklyndata.co>
2025-06-20 15:53:32 -0700

    Add safe cast macro

---
+++ integration_test_project/macros/cast.sql
@@ -0,0 +1,42 @@
+{% macro safe_cast(field, sql_type_or_logical) %}
+  {% set adapter = target.type %}
+
+  {% set logical_type_map = {
+    'string': {
+      'postgres': 'TEXT',
+      'sqlserver': 'VARCHAR',
+      'bigquery': 'STRING',
+      'snowflake': 'VARCHAR',
+      'databricks': 'STRING',
+      'trino': 'VARCHAR'
+    },
+    'numeric': {
+      'postgres': 'NUMERIC',
+      'sqlserver': 'FLOAT',
+      'bigquery': 'FLOAT64',
+      'snowflake': 'FLOAT',
+      'databricks': 'DOUBLE',
+      'trino': 'DOUBLE'
+    },
+    'date': {
+      'postgres': 'DATE',
+      'sqlserver': 'DATE',
+      'bigquery': 'DATE',
+      'snowflake': 'DATE',
+      'databricks': 'DATE',
+      'trino': 'DATE'
+    }
+  } %}
+
+  {% if execute %}
+    {% set resolved_type = (
+      api.Column.translate_type(sql_type_or_logical)
+      if api.Column is defined and api.Column.translate_type is defined
+      else logical_type_map.get(sql_type_or_logical, {}).get(adapter, sql_type_or_logical)
+    ) %}
+  {% else %}
+    {% set resolved_type = sql_type_or_logical %}
+  {% endif %}
+
+  CAST({{ field }} AS {{ resolved_type }})
+{% endmacro %}
