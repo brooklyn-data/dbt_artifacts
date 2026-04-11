b2320500bf0421df813e9cff957de262dbfd0e12
Zach Brown <zbrown@carrumhealth.com>
2026-04-11 11:24:52 -0400

    feat: add Athena dispatch variants for database helper macros

--- macros/upload_results/insert_into_metadata_table.sql
+++ macros/upload_results/insert_into_metadata_table.sql
@@ -84,3 +84,15 @@
     {% do run_query(insert_into_table_query) %}
 
 {%- endmacro %}
+
+{% macro athena__insert_into_metadata_table(relation, fields, content) -%}
+
+    {% set insert_into_table_query %}
+    insert into {{ relation }} {{ fields }}
+    values
+    {{ content }}
+    {% endset %}
+
+    {% do run_query(insert_into_table_query) %}
+
+{%- endmacro %}
